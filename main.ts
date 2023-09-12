49d285748a9425e6ef08a6d651f0ca6f0db8ad1a
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-09-12 13:11:02 -0400

    Linting Fix

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jack Eddy
+ * Created on: Sep 2023
+ * This program prints "Hello, Jack!" on a micro:bit
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, Jack!')
